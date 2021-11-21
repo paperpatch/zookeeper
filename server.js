76ec1f8b38544fbaca68800f8d2f3ca3a6e370cb
paperpatch <patchen21@gmail.com>
2021-11-20 22:11:07 -0500

    initial server.js function. created app.get and app.send

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const { animals } = require('./data/animals.json')
+
+app.get('/api/animals', (req, res) => {
+  res.send('Hello!');
+});
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+})
